abb5a945f8c587b6df01b61b102c9e5a59527405
Coby-Amar <cobyamar@gmail.com>
2024-07-03 14:03:39 -0700

    fix for git

--- src/services/properties.service.ts
+++ src/services/properties.service.ts
@@ -12,14 +12,8 @@ class PropertiesService {
     propertiesForSale: Property[] = []
     subPropertyTypes: string[] = []
 
-    // constructor() {
-    //     this.__init()
-    // }
-
     async loadProperties() {
-        const result = await fetch('https://app.nadlanvip.com/api/public/properties', {
-            mode: 'cors',
-        })
+        const result = await fetch('api/public/properties')
         const jsonResult:PropertyInterface[] = await result.json()
         const subtypes: Dict = {}
         this.allProperties = jsonResult.map(property => {
@@ -33,22 +27,6 @@ class PropertiesService {
         this.propertiesForRent = this.allProperties.filter(property => property.transaction_type.includes(TransactionType.RENT))
     }
     
-    private async __init()  {
-        const result = await fetch('https://app.nadlanvip.com/api/public/properties', {
-            mode: 'cors',
-        })
-        const jsonResult:PropertyInterface[] = await result.json()
-        const subtypes: Dict = {}
-        this.allProperties = jsonResult.map(property => {
-            if (!subtypes[property.sub_type]) {
-                subtypes[property.sub_type] = true
-            }
-            return Property.fromJson({...property, id: uuid()})
-        })
-        this.subPropertyTypes = Object.keys(subtypes)
-        this.propertiesForSale = this.allProperties.filter(property => property.transaction_type.includes(TransactionType.SELL))
-        this.propertiesForRent = this.allProperties.filter(property => property.transaction_type.includes(TransactionType.RENT))
-    }
 }
 
 export const propertiesService = new PropertiesService()
