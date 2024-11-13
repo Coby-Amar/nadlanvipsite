e7787f143c002834ababfdedc4667c9d705ced11
Coby-Amar <cobyamar@gmail.com>
2024-11-13 00:08:27 -0800

    cleaned code

--- src/views/Contact.view.tsx
+++ src/views/Contact.view.tsx
@@ -1,9 +1,7 @@
 export default function Contact() {
     return (
-        <>
         <div className="contact-iframe">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd_yXWwVp6ORYAkAQY6SosEwSSnxnXPwXz7zHK0QChFFPn1gQ/viewform?embedded=true" width="640" height="1100" >Loading…</iframe>
         </div>
-        </>
     )
 }
